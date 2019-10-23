cf35e2a295a86412c19052b5514b40d56ebda65a
Damien Erambert <damien.erambert@me.com>
2019-10-23 12:41:53 -0700

    use mpc only for now

--- widgets/lumiere-music.jsx
+++ widgets/lumiere-music.jsx
@@ -1,4 +1,7 @@
-export const command = `osascript lumiere/current-track.applescript`
+// Use the command below if you want mpc/iTunes/Spotify
+// Note that it might increase CPU usage because AppleScript is slow AF.
+// export const command = `osascript lumiere/current-track.applescript`
+export const command = "/usr/local/bin/mpc current --format='%artist% - %title%'"
 export const render = ({ output }) => {
   window.LumiereMusic = output
 }
