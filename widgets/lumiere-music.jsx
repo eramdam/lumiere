77f3d4bc4ad3149556f4d39bed081c6ce5f8829f
Damien Erambert <damien.erambert@me.com>
2019-12-06 13:22:14 -0800

    re-enabled mpd/spotify/itunes

--- widgets/lumiere-music.jsx
+++ widgets/lumiere-music.jsx
@@ -1,7 +1,8 @@
 // Use the command below if you want mpc/iTunes/Spotify
 // Note that it might increase CPU usage because AppleScript is slow AF.
 // export const command = `osascript lumiere/current-track.applescript`
-export const command = "/usr/local/bin/mpc current --format='%artist% - %title%'"
+// export const command = "/usr/local/bin/mpc current --format='%albumartist% - %title%'"
+export const command = "osascript lumiere/current-track.applescript"
 export const render = ({ output }) => {
   window.LumiereMusic = output
 }
